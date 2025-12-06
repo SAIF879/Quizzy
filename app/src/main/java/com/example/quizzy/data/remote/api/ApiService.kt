f67f27d74b09b47f804014ef1e2ed1bb159740d9
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2025-12-06 08:45:14 +0530

    feat: Add Retrofit API service for student dashboard
    Adds the `ApiService` interface with a suspend function to fetch student dashboard data.
    
    This includes:
    - A `GET` request definition for the dashboard endpoint.
    - Query parameters `alt` and `token` for the request.
    - A response type of `Response<ResultDto>`.

--- app/src/main/java/com/example/quizzy/data/remote/api/ApiService.kt
+++ app/src/main/java/com/example/quizzy/data/remote/api/ApiService.kt
@@ -1,2 +1,17 @@
 package com.example.quizzy.data.remote.api
 
+import com.example.quizzy.data.remote.dto.ResultDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService{
+
+    @GET("v0/b/user-contacts-ade83.appspot.com/o/student_dashboard.json")
+    suspend fun getDashboardData(
+        @Query("alt") alt: String = "media",
+        @Query("token") token: String = "0091b4c2-2ee2-4326-99cd-96d5312b34bd"
+    ): Response<ResultDto>
+
+
+}
